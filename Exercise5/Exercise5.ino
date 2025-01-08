3c419a2ed58288f94d532710f16ce9f62b315d24
Mikkel-Svendsen <mikkel.svendsen15@gmail.com>
2025-01-08 11:28:48 +0100

    Solved Exercise 5 and added image of 5f

--- Exercise5/Exercise5.ino
+++ Exercise5/Exercise5.ino
@@ -1,15 +1,24 @@
 int incomingByte = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  //Initialize the serial communication
   Serial.begin(9600);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //The if statement checks wether a byte is available to read
   if (Serial.available() > 0) {
+    //When a byte is available to be read, it is read and put into incomingbyte
     incomingByte = Serial.read();
+
+    //It prints the recieved byte.
     Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
+    //Serial.println(incomingByte, DEC);
+    Serial.println((char)incomingByte);
   }
 }
+
+// 5d. The recieved values are ASCII codes.
+// 5e. When sending a line ending, the ASCII code for the line ending is also sent.
+// 5f. The ASCII values are shown as their proper symbols.
